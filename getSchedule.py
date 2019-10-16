d1c5304d11227bbd2a6013fe54c15e14b25ba614
t-n-02 <56138607+t-n-02@users.noreply.github.com>
2019-10-15 23:32:14 -0400

    Update getSchedule.py

--- getSchedule.py
+++ getSchedule.py
@@ -1,17 +1,4 @@
 import urllib.request
-from html.parser import HTMLParser
-
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
-
-parser = MyHTMLParser()
 
 file = open("out.txt", "w")
 
